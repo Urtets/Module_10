3fd7bf85b6fc7ecfcc91fc135ff2047708988375
Юрий <urtets@gmail.com>
2024-11-08 11:20:06 +0300

    Module_10 Homework

---
+++ module_10_1.py
@@ -0,0 +1,28 @@
+import time
+import threading
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding="utf-8") as writer:
+        for i in range(word_count):
+
+            some_words = f'Секретное слово № {i}'
+            writer.write(some_words)
+            time.sleep(0.1)
+            print(threading.current_thread().is_alive())
+    print(f"Завершилась запись в файл {file_name}")
+
+
+# write_words(10, "example1.txt")
+# write_words(30, "example2.txt")
+# write_words(200, "example3.txt")
+# write_words(100, "example4.txt")
+
+thread_1 = threading.Thread(target=write_words(10, "example5.txt"))
+thread_2 = threading.Thread(target=write_words(30, "example6.txt"))
+thread_3 = threading.Thread(target=write_words(200, "example7.txt"))
+thread_4 = threading.Thread(target=write_words(100, "example8.txt"))
+thread_1.start()
+thread_2.start()
+thread_3.start()
+thread_4.start()
+thread_1.join()
