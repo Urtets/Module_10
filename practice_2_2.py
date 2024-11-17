b44b4f358c762506c9dc98b535228a8a0caa988c
Юрий <urtets@gmail.com>
2024-11-17 16:22:40 +0300

    Module_10 Homework

---
+++ practice_2_2.py
@@ -0,0 +1,10 @@
+import  pprint
+import requests
+from threading import Thread
+
+
+ACCESS_TOKEN = 'CXyFeSBw2lAdG41xkuU3LS6a_nwyxwwCz2dCkUohw-rw0C49x2HqP__6_4is5RPx'
+RANDOM_GENRE_API_URL = 'https://binaryjazz.us/wp-json/generator/v1/genre/'
+GENIUS_API_URL = 'https://api.genius.com/search'
+GENIUS_URL = 'https://genius.com'
+
